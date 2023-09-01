e661ea795a87ab45b2871228485335556874a371
Mr-1504 <128713637+Mr-1504@users.noreply.github.com>
2023-09-01 22:16:45 +0700

    laptrinhonline

---
+++ nqsontinhtong2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    int n, x;
+    cin >> n >> x;
+    unordered_set<int> b;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin>>a[i];
+        if (b.count(x - a[i]) && x - a[i] != a[i]) {
+            cout << min(a[i], x - a[i]) << " " << max(a[i], x - a[i]) << endl;
+        }
+        b.insert(a[i]);
+    }
+
+    return 0;
+}
