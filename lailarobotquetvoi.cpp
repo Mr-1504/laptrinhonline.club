7a321cf7a9ac239699e602b99b8104312767c50b
Mr-1504 <128713637+Mr-1504@users.noreply.github.com>
2023-10-08 09:35:41 +0700

    Update lailarobotquetvoi.cpp

--- lailarobotquetvoi.cpp
+++ lailarobotquetvoi.cpp
@@ -3,13 +3,13 @@ using namespace std;
 #define endl "\n"
 #define ll long long
 const int MOD = 1e9 + 7;
-int dp[30001];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int n, m;
     cin >> n >> m;
+    unordered_map<ll, int> dp;
     int a, b;
     while (n--)
     {
